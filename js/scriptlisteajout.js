1ace4b4751ea78fc16c47bd2786320812fb97aaa
walimkr <islemmseddi@gmail.com>
2019-01-14 16:14:20 +0100

    update pages admin

---
+++ js/scriptlisteajout.js
@@ -0,0 +1,10 @@
+
+document.getElementById('button1').onclick = function () {
+    document.getElementById('liste').style.display="block";
+    document.getElementById('Form').style.display="none";
+}
+
+document.getElementById('button2').onclick = function () {
+    document.getElementById('liste').style.display="none";
+    document.getElementById('Form').style.display="block";
+}
